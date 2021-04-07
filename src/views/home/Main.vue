cc05fdc500b8f38f832e3c7ccf0f951ed48c44d3
胡超宇 <huchaoyu@zhongbiao.mobi>
2021-04-07 13:09:18 +0800

    add echarts doc url

--- src/views/home/Main.vue
+++ src/views/home/Main.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="home-main" @mousewheel="mouseWheelHandler">
-    <div class="panel-container"
-      :style="panel_transform"
-    >
-      <div class="panel"
+    <div class="panel-container" :style="panel_transform">
+      <div
+        class="panel"
         v-for="(p, panel_page_num) in render_items"
         :key="panel_page_num"
         :style="panel_width"
       >
-        <transition-group
-          tag="div"
-          name="box"
-        >
-          <div class="cell"
-            :class="{'main-cell': i === 0, 'right-main-cell': i === 7}"
+        <transition-group tag="div" name="box">
+          <div
+            class="cell"
+            :class="{ 'main-cell': i === 0, 'right-main-cell': i === 7 }"
             v-for="(item, i) in panel_page_num === page ? p : []"
             :key="item + i"
           >
-            <div class="box"
+            <div
+              class="box"
               :style="box_style(item, page, i)"
               @click="clickHandler(item, page, i)"
-            >{{item.label}}</div>
+            >
+              <i v-if="item.url" class="el-icon-link"></i> {{ item.label }}
+            </div>
           </div>
         </transition-group>
       </div>
@@ -36,18 +36,38 @@ export default {
       page: 0,
       items: [
         { label: 'Lodash 文档', url: 'https://www.lodashjs.com/docs/latest' },
-        { label: 'Element UI', url: 'https://element.eleme.cn/#/zh-CN/component/installation' },
+        {
+          label: 'Element UI',
+          url: 'https://element.eleme.cn/#/zh-CN/component/installation'
+        },
+        {
+          label: 'Echarts 配置项文档',
+          url: 'https://echarts.apache.org/zh/option.html'
+        },
+        {
+          label: 'Echarts Gallery',
+          url:
+            'https://www.makeapie.com/explore.html#sort=rank~timeframe=all~author=all'
+        },
         // { label: 'Jenkins', url: 'http://101.201.42.70:8080/' },
         // { label: '中建华北项目', url: 'http://cscec1bhb.marketbox.cn/bulletin' },
         // { label: '国祯环保', url: 'xx' },
-        { label: '易众标小程序', modal: 'ShowQrcode', data: { codes: [ '//www.baidu.com/img/bd_logo1.png?where=super' ] } },
+        {
+          label: '易众标小程序',
+          modal: 'ShowQrcode',
+          data: { codes: ['//www.baidu.com/img/bd_logo1.png?where=super'] }
+        },
         { label: '易众标PC', url: 'https://www.ezhongbiao.com/' },
         { label: '慧聪系列公众号' },
         { label: '经营决策平台' },
         { label: '啄木鸟医疗营销平台' },
         { label: '数纽知识云', url: 'http://kc.zhongbiao.mobi/' },
         { label: '数据运营平台', url: 'http://eagle.zhongbiao.mobi/' },
-        { label: '厦门可视化大屏', url: 'http://youe.zhongbiao.mobi/client-charts/#/', bak_url: 'http://117.29.164.46:8081/client-charts/#/' },
+        {
+          label: '厦门可视化大屏',
+          url: 'http://youe.zhongbiao.mobi/client-charts/#/',
+          bak_url: 'http://117.29.164.46:8081/client-charts/#/'
+        },
         { label: '福州台江区政策服务平台' },
         { label: '福州市12345热线数据研判平台' },
         { label: '中建一局报告自动化' }
@@ -55,24 +75,24 @@ export default {
     }
   },
   computed: {
-    render_items: vm => chunk(vm.items.concat(vm.items).concat(vm.items), 10),
-    page_total: vm => vm.render_items.length,
+    render_items: (vm) => chunk(vm.items.concat(vm.items).concat(vm.items), 10),
+    page_total: (vm) => vm.render_items.length,
     // 计算滑动距离
-    panel_transform: vm => {
+    panel_transform: (vm) => {
       return {
-        width: (vm.page_total * 100) + '%',
-        transform: `translateX(-${(100 / vm.page_total * vm.page) + '%'})`
+        width: vm.page_total * 100 + '%',
+        transform: `translateX(-${(100 / vm.page_total) * vm.page + '%'})`
       }
     },
     // 每个panel的宽度
-    panel_width: vm => ({ width: (100 / vm.page_total) + '%' })
+    panel_width: (vm) => ({ width: 100 / vm.page_total + '%' })
   },
   methods: {
     clickHandler (item, page, i) {
       if (item.url) {
         this.openNewTab(item.url)
       } else if (item.modal) {
-        this.$modal(item.modal, { data: item.data }).then(x => x)
+        this.$modal(item.modal, { data: item.data }).then((x) => x)
       }
     },
     openNewTab (url) {
@@ -82,18 +102,23 @@ export default {
       elem.click()
       elem.remove()
     },
-    mouseWheelHandler: throttle(function (evt) {
-      if (evt.wheelDelta < 0 && this.page < this.page_total - 1) {
-        this.page++
-      } else if (evt.wheelDelta >= 0 && this.page > 0) {
-        this.page--
-      }
-    }, 2000, { leading: true, trailing: false }),
+    mouseWheelHandler: throttle(
+      function (evt) {
+        if (evt.wheelDelta < 0 && this.page < this.page_total - 1) {
+          this.page++
+        } else if (evt.wheelDelta >= 0 && this.page > 0) {
+          this.page--
+        }
+      },
+      1000,
+      { leading: true, trailing: false }
+    ),
     box_style (item, page, i) {
       let cache_key = [item.label, page, i].join('_')
       box_style_cache = box_style_cache || {}
       if (!box_style_cache[cache_key]) {
-        box_style_cache[cache_key] = '#' + (~~(Math.random() * (1 << 24))).toString(16)
+        box_style_cache[cache_key] =
+          '#' + (~~(Math.random() * (1 << 24))).toString(16)
       }
       return {
         'background-color': box_style_cache[cache_key]
@@ -106,17 +131,23 @@ export default {
 $padding: 15px;
 $height: 200px;
 .home-main {
-  .box-enter-active, .box-leave-active, .box-move {
+  .box-enter-active,
+  .box-leave-active,
+  .box-move {
     transition: all 1s;
   }
   .box-enter, .box-leave-to
   /* .list-leave-active for below version 2.1.8 */ {
     opacity: 0;
-    transform: translate3d(100px, 0, 0) scale(.5);
+    transform: translate3d(100px, 0, 0) scale(0.5);
     transform-origin: right center;
   }
   .cell {
-    @for $i from 1 through 17 { &:nth-child(#{$i}) { transition-delay: $i * 0.03s; } }
+    @for $i from 1 through 17 {
+      &:nth-child(#{$i}) {
+        transition-delay: $i * 0.03s;
+      }
+    }
   }
   padding: $padding;
   width: 100%;
@@ -127,7 +158,7 @@ $height: 200px;
     height: 100%;
     box-sizing: border-box;
     float: left;
-    transition: all .3s;
+    transition: all 0.3s;
   }
   .panel {
     height: 100%;
@@ -163,7 +194,7 @@ $height: 200px;
     color: #fff;
     font-size: 30px;
     cursor: pointer;
-    transition: all .3s;
+    transition: all 0.3s;
     user-select: none;
     &:hover {
       box-shadow: 0 0 10px 10px #bbb;
